a81b175aeeaa7ff1336eed0dcffc630b6ffa4ffb
matefernandezcc <matefernandezc@gmail.com>
2026-06-12 15:46:53 -0300

    💹 Stocks App

---
+++ android/app/src/main/java/com/stocks/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.stocks.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./www/**/*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        // Custom premium colors if needed
+      }
+    },
+  },
+  plugins: [],
+}
